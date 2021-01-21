47c758ad75aaaac30b29a674510b695c1750b27a
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-21 20:56:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 board = ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "]
 
 def display_board(board)
-  print "#{board[0]} | #{board[1]} | #{board[2]}"
+  print " #{board[0]} | #{board[1]} | #{board[2]}"
 
 end
